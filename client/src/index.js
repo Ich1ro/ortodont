1b500d75b1da794b6e0e181a6719f945ff6d9209
ich1ro <pavel.dubenchuk.02@gmail.com>
2023-12-08 22:50:07 +0200

    update the front

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
@@ -8,11 +8,9 @@ import { router } from './router';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+	<Provider store={store}>
 		<RouterProvider router={router} />
 	</Provider>
 );
-
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,20 +1,68 @@
 import { createBrowserRouter } from 'react-router-dom';
-import App from '../App';
+import { Login, Main, Consultations, Profile } from '../pages';
+import { AdminLogin, ClientLogin, AddOns, Discounts, Locations, PracticeInfo, Treatments, Users, Info, ResetPassword } from '../components';
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />,
-        children: [
-            {
-                path: '/login',
-                element: <App />
-            },
-            {
-                path: '/register',
-                element: <App />
-            },
-        ]
-    },
-
-])
+	{
+		path: '/',
+		element: <Main />,
+		children: [
+			{
+				path: '/info',
+				element: <Info />,
+				children: [
+					{
+						path: '/info/practice-information',
+						element: <PracticeInfo />
+					},
+					{
+						path: '/info/treatments',
+						element: <Treatments />
+					},
+					{
+						path: '/info/add-ons',
+						element: <AddOns />
+					},
+					{
+						path: '/info/discounts',
+						element: <Discounts />
+					},
+					{
+						path: '/info/locations',
+						element: <Locations />
+					},
+					{
+						path: '/info/users',
+						element: <Users />
+					}
+				]
+			},
+			{
+				path: '/consultations',
+				element: <Consultations />
+			},
+			{
+				path: '/profile',
+				element: <Profile />
+			}
+		]
+	},
+	{
+		path: '/login',
+		element: <Login />,
+		children: [
+			{
+				path: '/login/admin',
+				element: <AdminLogin />
+			},
+			{
+				path: '/login/bp',
+				element: <ClientLogin />
+			},
+			{
+				path: '/login/reset',
+				element: <ResetPassword />
+			}
+		]
+	}
+]);
